e721d0c765aef7bda108e00057074d624439924a
HARON <haronur@gmail.com>
2020-05-11 22:41:04 +0600

    04-Named routes in Vue

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 import Home from '../components/Home.vue';
+import About from '../components/About.vue';
 import Register from '../components/Register.vue';
 import Login from '../components/Login.vue';
 import Student from '../components/Student.vue'; 
@@ -12,9 +13,10 @@ const router = new Router({
 	mode: 'history',
     routes: [
     { path: '/', component: Home },
+    { path: '/about', component: About, name: 'about' },
     { path: '/register', component: Register },
     { path: '/login', component: Login },
-    { path: '/student/:id', component: Student },
+    { path: '/student/:id', component: Student, name: 'student' },
     ]
 });
 
